ec8f355e7f9ba375d04d783e900b17fcf6ff1022
Wellington Candil <wcandil34@gmail.com>
2021-12-07 10:16:37 -0300

    Camada de serviço e registro de componente

--- src/main/java/com/example/course/resources/UserResource.java
+++ src/main/java/com/example/course/resources/UserResource.java
@@ -1,20 +1,35 @@
 package com.example.course.resources;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.course.entities.User;
+import com.example.course.service.UserService;
 
 @RestController
 @RequestMapping(value= "/users")
 public class UserResource {
 	
+	@Autowired
+	UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Wellington", "wcandil34@gmail.com", "11908782983", "12352");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Optional<User>> findById(@PathVariable Long id){
+		Optional<User> obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 	
 }
---
+++ src/main/java/com/example/course/service/UserService.java
@@ -0,0 +1,24 @@
+package com.example.course.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.course.entities.User;
+import com.example.course.repositories.IUserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	IUserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public Optional<User> findById(Long id) {
+		return repository.findById(id);
+	}
+}
