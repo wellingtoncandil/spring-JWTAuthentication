321d7e6811292ee5a5bd8426720b5bfc4e8502e8
Wellington Candil <wcandil34@gmail.com>
2021-12-07 20:24:22 -0300

    Entidade product

--- src/main/java/com/example/course/config/TestConfig.java
+++ src/main/java/com/example/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.example.course.entities.Category;
 import com.example.course.entities.Order;
+import com.example.course.entities.Product;
 import com.example.course.entities.User;
 import com.example.course.entities.enums.OrderStatus;
 import com.example.course.repositories.ICategoryRepository;
 import com.example.course.repositories.IOrderRepository;
+import com.example.course.repositories.IProductRepository;
 import com.example.course.repositories.IUserRepository;
 
 @Configuration // informa que essa é uma classe de configuração
@@ -26,6 +28,8 @@ public class TestConfig implements CommandLineRunner{ //classe de configuração
 	private IOrderRepository orderRepository;
 	@Autowired
 	private ICategoryRepository categoryRepository;
+	@Autowired
+	private IProductRepository productRepository;
 
 	@Override
 	public void run(String... args) throws Exception { //tudo que estiver dentro do método run será executado ao iniciar a aplicação
@@ -41,8 +45,15 @@ public class TestConfig implements CommandLineRunner{ //classe de configuração
 		Category c2 = new Category(null, "Books");
 		Category c3 = new Category(null, "Computers"); 
 		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+		
 		userRepository.saveAll(Arrays.asList(u1, u2));//salva os usuarios no bd passando-os como paramentro para uma lista
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		categoryRepository.saveAll(Arrays.asList(c1, c2, c3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 	}
 }
---
+++ src/main/java/com/example/course/service/ProductService.java
@@ -0,0 +1,25 @@
+package com.example.course.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.course.entities.Product;
+import com.example.course.repositories.IProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	IProductRepository repository;
+
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+	
+	public Optional<Product> findById(Long id) {
+		return repository.findById(id);
+	}
+}
