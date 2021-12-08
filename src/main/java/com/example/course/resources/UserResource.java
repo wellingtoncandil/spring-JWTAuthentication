365773105788d3b960ec161a538394f01d3f7db4
Wellington Candil <wcandil34@gmail.com>
2021-12-08 11:44:48 -0300

    Insersão de usuário no banco

--- src/main/java/com/example/course/resources/UserResource.java
+++ src/main/java/com/example/course/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.example.course.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.course.entities.User;
 import com.example.course.service.UserService;
@@ -30,6 +34,13 @@ public class UserResource {
 	public ResponseEntity<Optional<User>> findById(@PathVariable Long id){
 		Optional<User> obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){//annotation que informa a chegada do objeto em modo JSON para que seja desserializado para um obj java
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();//cria uma URL para o obj inserido
+		return ResponseEntity.created(uri).body(obj); //mostra o obj criado e o local (url) do mesmo
 	}
 	
 }
--- src/main/java/com/example/course/service/UserService.java
+++ src/main/java/com/example/course/service/UserService.java
@@ -21,4 +21,8 @@ public class UserService {
 	public Optional<User> findById(Long id) {
 		return repository.findById(id);
 	}
+	
+	public User insert(User user) {
+		return repository.save(user);
+	}
 }
