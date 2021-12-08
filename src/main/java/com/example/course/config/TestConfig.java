e7be4db93f6ec23894c217a4a962caa8caeab7ba
Wellington Candil <wcandil34@gmail.com>
2021-12-08 10:47:00 -0300

    Entidade Payment e associação OneToOne

--- src/main/java/com/example/course/config/TestConfig.java
+++ src/main/java/com/example/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.example.course.entities.Category;
 import com.example.course.entities.Order;
 import com.example.course.entities.OrderItem;
+import com.example.course.entities.Payment;
 import com.example.course.entities.Product;
 import com.example.course.entities.User;
 import com.example.course.entities.enums.OrderStatus;
@@ -43,7 +44,7 @@ public class TestConfig implements CommandLineRunner{ //classe de configuração
 		
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.PAID, u1);
 		
 		Category c1 = new Category(null, "Electronics");
 		Category c2 = new Category(null, "Books");
@@ -75,5 +76,10 @@ public class TestConfig implements CommandLineRunner{ //classe de configuração
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay = new Payment(null, Instant.parse("2019-07-22T16:21:22Z"), o3);
+		o3.setPayment(pay);
+		
+		orderRepository.save(o3);
 	}
 }
--- src/main/java/com/example/course/entities/Order.java
+++ src/main/java/com/example/course/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.example.course.entities.enums.OrderStatus;
@@ -39,6 +41,9 @@ public class Order implements Serializable{
 
 	@OneToMany(mappedBy= "id.order") // o OrderItem contem o "id" que por sua vez contem o order
 	private Set<OrderItem> items = new HashSet<>();
+	
+	@OneToOne(mappedBy="order", cascade = CascadeType.ALL)//mapeamento das duas entidades para que tenham o mesmo ID
+	private Payment payment;
 	
 	public Order() {
 	}
@@ -87,6 +92,14 @@ public class Order implements Serializable{
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
 	}
 
 	@Override
